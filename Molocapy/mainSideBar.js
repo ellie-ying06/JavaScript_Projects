b8a9965015ef928fae958eda97f1b47e63fed3aa
ellie-ying06 <chenpeiying06@gmail.com>
2026-07-15 21:03:35 +0800

    Create mainSideBar.js

---
+++ Molocapy/mainSideBar.js
@@ -0,0 +1,8 @@
+function showmainSidebar() {
+                    const mainSideBar = document.querySelector('.mainSideBar')
+                    mainSideBar.style.display = 'flex'
+}
+function hidemainSidebar() {
+                    const mainSideBar = document.querySelector('.mainSideBar')
+                    mainSideBar.style.display = 'none'
+}     
