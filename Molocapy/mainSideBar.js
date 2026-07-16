24397509f266d76d3422d6ca37df9d91811dc31d
ellie-ying06 <chenpeiying06@gmail.com>
2026-07-16 09:36:17 +0800

    Delete Molocapy directory

--- Molocapy/mainSideBar.js
+++
@@ -1,8 +0,0 @@
-function showmainSidebar() {
-                    const mainSideBar = document.querySelector('.mainSideBar')
-                    mainSideBar.style.display = 'flex'
-}
-function hidemainSidebar() {
-                    const mainSideBar = document.querySelector('.mainSideBar')
-                    mainSideBar.style.display = 'none'
-}     
